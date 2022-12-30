3df0b1d68ddecbb4637617aa8003ce27e8d6e251
Yusuke Wada <yusuke@kamawada.com>
2022-12-30 21:40:59 +0900

    add an example

---
+++ examples/basic/src/client.mts
@@ -0,0 +1,20 @@
+import { Client } from '@hono/client'
+import type { AppType } from './server'
+
+const client = new Client<AppType>('http://localhost:8787/api')
+
+const res = await client.post('/posts', {
+  json: {
+    id: 123,
+    title: 'Hello Hono!',
+    published: true,
+  },
+})
+
+if (!res.ok) {
+  throw new Error('Not OK!')
+}
+
+const data = await res.json()
+
+console.log(`${data.message}`)
---
+++ examples/basic/src/server.ts
@@ -0,0 +1,41 @@
+import { Hono } from 'hono'
+import { validator } from 'hono/validator'
+import { z } from 'zod'
+
+const api = new Hono()
+
+const schema = z.object({
+  id: z.number(),
+  title: z.string(),
+  published: z.boolean(),
+})
+
+const route = api
+  .post(
+    '/posts',
+    validator('json', (value, c) => {
+      const result = schema.safeParse(value)
+      if (!result.success) {
+        return c.json({
+          success: false,
+          message: 'Invalid!',
+        })
+      }
+      return result.data
+    }),
+    (c) => {
+      const { title, published } = c.req.valid()
+      return c.jsonT({
+        success: true,
+        message: `"${title}" is ${published ? 'published' : 'not published'}`,
+      })
+    }
+  )
+  .build()
+
+export type AppType = typeof route
+
+const app = new Hono()
+app.route('/api', api)
+
+export default app
