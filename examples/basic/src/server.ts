65693e42dba1f899c7300fca64937eb43639dde7
Yusuke Wada <yusuke@kamawada.com>
2023-01-02 23:24:50 +0900

    example: use `zod-validator` middleware

--- examples/basic/src/server.ts
+++ examples/basic/src/server.ts
@@ -1,5 +1,5 @@
+import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
-import { validator } from 'hono/validator'
 import { z } from 'zod'
 
 const api = new Hono()
@@ -11,23 +11,13 @@ const schema = z.object({
 })
 
 const route = api
-  .post(
-    '/posts',
-    validator('json', (value, c) => {
-      const result = schema.safeParse(value)
-      if (!result.success) {
-        return c.text('Invalid!', 400)
-      }
-      return result.data
-    }),
-    (c) => {
-      const { title, published } = c.req.valid()
-      return c.jsonT({
-        success: true,
-        message: `"${title}" is ${published ? 'published' : 'not published'}`,
-      })
-    }
-  )
+  .post('/posts', zValidator('json', schema), (c) => {
+    const { title, published } = c.req.valid()
+    return c.jsonT({
+      success: true,
+      message: `"${title}" is ${published ? 'published' : 'not published'}`,
+    })
+  })
   .build()
 
 export type AppType = typeof route
